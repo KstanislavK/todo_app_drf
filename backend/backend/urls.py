5efa22bc3a90f6781daf11cae83bc28cbc4a6c42
KstanislavK <t7s9SF2FWTQfXxL>
2022-03-16 14:37:31 +0300

    les_9

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 from userapp.views import UserModelViewSet
 from todoapp.views import ProjectViewSet, TODOViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='TODOApi',
+        default_version='1.0',
+        description='docs for...',
+        contact=openapi.Contact(email='admin@ex.local'),
+        license=openapi.License(name='KSK'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
@@ -30,4 +46,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', obtain_auth_token),
+    path('swagger/', schema_view.with_ui('swagger')),
+    path('redoc/', schema_view.with_ui('redoc')),
 ]
--- backend/userapp/views.py
+++ backend/userapp/views.py
@@ -1,10 +1,14 @@
 from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
 from .models import User
-from .serizalizers import UserModelSerializer
+from .serizalizers import UserModelSerializer, UserModelFullSerializer
 
 
 class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
                        viewsets.GenericViewSet):
-    serializer_class = UserModelSerializer
     queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.version == '2.0':
+            return UserModelFullSerializer
+        return UserModelSerializer
